db59f86a616c597317ae4f6961d6eebcdad29075
zac <827167689@qq.com>
2022-07-28 12:12:38 +0000

    Add.editorconfig

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Zack</div>
+                <div class="title">Hello word! - by Zack</div>
             </div>
         </div>
     </body>
