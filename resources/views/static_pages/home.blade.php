a18936ec2d09023dc568b715c736a5f6f66928bc
zac <827167689@qq.com>
2022-07-29 07:08:16 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -11,7 +11,7 @@
       一切,将从这里开始,欢迎来到菜鸟的世界!
    </p>
    <p>
-    <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
    </p>
  </div>
  @stop
