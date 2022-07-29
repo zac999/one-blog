2f2eff6282364722ecdde547c72890f387093387
zac <827167689@qq.com>
2022-07-29 06:38:40 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <img class="brand-icon"
+  src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://www.csdn.net/" target=_blank class="text-decoration-none">
+    csdn -- 花里胡哨退退退...
+  </a>
+
+  <div class="float-end">
+    <a href="/about">关于</a>
+  </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
+   <a class="navbar-brand" href="/">Fuck</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+        </div>
+</nav>
