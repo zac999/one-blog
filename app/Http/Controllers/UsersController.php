c4340f714150a2e027e859f7ebaf46e5e40243fd
zac <827167689@qq.com>
2022-07-30 03:03:11 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    //create
     public function create(){
         return view('users.create');
+    }
+
+    //show
+    public function show(User $user){
+        return view('users.show',compact('user'));
     }
 }
