fdd314f09b3132bb33358437c347a2d3f7f57251
zac <827167689@qq.com>
2022-08-01 02:54:30 +0000

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -22,8 +22,13 @@
             <label for="password">密码：</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
           </div>
-
-          <button type="submit" class="btn btn-primary">登录</button>
+<div class="from-group">
+  <div class="from-check">
+    <input type="checkbox" class="from-check-input" name="remember" id="exampleCheck1">
+    <label class="from-check-label" for="exampleCheck1">记住我</label>
+  </div>
+</div>
+        <button type="submit" class="btn btn-primary">登录</button>
       </form>
 
       <hr>
